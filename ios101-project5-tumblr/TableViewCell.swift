86125c57f3058a1969a3d051258aaf94a7010081
Karelle Hofler <karellehofler@gmail.com>
2025-04-03 02:55:06 -0400

    Post image and summary added to table view

---
+++ ios101-project5-tumblr/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  ios101-project5-tumblr
+//
+//  Created by Karelle Hofler on 4/2/25.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
